5f3b7fb219b8a586a8dd106242dea0d2ac0eeffd
nnnnneg <mikunegi.cs@gmail.com>
2018-07-06 01:39:36 +0900

    fix_heroku

--- db/migrate/20180609173040_remove_integer_to_payments.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIntegerToPayments < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :payments, :integer, :string
-  end
-end
--- db/migrate/20180610061158_remove_from_to_paryment.rb
+++ db/migrate/20180610061158_remove_from_to_paryment.rb
@@ -1,6 +1,8 @@
 class RemoveFromToParyment < ActiveRecord::Migration[5.1]
   def change
-    remove_column :payments, :from, :integer
-    remove_column :payments, :to, :integer
+    remove_column :payments, :from_id, :integer
+    remove_column :payments, :to_id, :integer
+    remove_column :payments, :payment_id, :integer
+
   end
 end
